48b4d9b7a0672055d0f58b8b53b1055f1e79b155
Igor Simko <simko.igor@gmail.com>
2017-01-20 00:33:07 +0100

    Add function call by name

--- src/command.js
+++ src/command.js
@@ -2,24 +2,40 @@ var apiai = require('apiai');
 var crypto = require('crypto');
 var app = require('express');
 var request = require('request');
+var fs = require('fs');
 
 var sha = crypto.createHash('sha256');
 var sessionId = sha.update(Math.random().toString()).digest('hex');
 
-console.log("SessionID=["+sessionId+"]");
-
 var app_ai = apiai("fe22179c6de74a429bc43857a69e2dfa");
+var actions = JSON.parse(fs.readFileSync('src/actions.json', 'utf8'));
 
 var options = {
     sessionId: sessionId
 }
 
-function callFunctionByName(name){
-    name();
+module.exports = {
+  callFunctionByName: function (input) {
+    var functionName = input.toString().trim()
+    if (functionName == undefined) {
+        return;
+    }
+    var callFunction = getRealFunctionCall(functionName);
+    console.log("Calling function by name ["+ functionName +"]");
+    global[callFunction]();
+  }
+};
+
+function getRealFunctionCall(key){
+
+        console.log(key);
+
+    console.log("ac:" +actions.thing[key]);
+    console.log(actions);
 }
 
 // get methods
-function getTemperatureInfo(){
+global.getTemperatureInfo = function getTemperatureInfo(){
     request.post(
         'http://api.openweathermap.org/data/2.5/weather?q=Bratislava&APPID=25d58ec4b67cf6971203cf044ceda2ec&units=metric',
         function (error, response, body) {
--- src/server.js
+++ src/server.js
@@ -4,11 +4,12 @@ var apiai = require('apiai');
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var app = express();
+var command = require('./command.js');
 
 console.log("Initializing server...");
 
 var sha = crypto.createHash('sha256');
-var sessionId = sha.update(Math.random().toString()).digest('hex');
+var sessionId = sha.update(Math.random().toString()).digest('hex').substring(0,20);
 console.log("SessionID=["+sessionId+"]");
 
 var app_ai = apiai("fe22179c6de74a429bc43857a69e2dfa");
@@ -16,21 +17,35 @@ var app_ai = apiai("fe22179c6de74a429bc43857a69e2dfa");
 var options = {
     sessionId: sessionId
 }
-
-
 app.use(express['static'](__dirname ));
 app.use(bodyParser.urlencoded({
     extended: true
-}));
+    })
+);
 
 app.post('/cmd', function(req, res){
-    console.log('POST\tcmd');
-    console.log(req.body);
+    console.log('POST\t/cmd');
+    var text;
+    if (req.body != undefined) {
+        text = req.body.text;
+        //console.log('BODY\ttext: '+ req.body.text);
+    } else if(text != undefined){
+        console.log('ERROR\t body.text is undefined');
+        errorResponse(res, "Body.text is undefined");
+    }
 
-    var request = app_ai.textRequest('What temperature is?', options);
+    var request = app_ai.textRequest(text, options);
 
     request.on('response', function(response) {
-        console.log(response);
+        if (response.status.code == 200) {
+            if (response.result != undefined || response.result.size() > 0) {
+                var action = response.result.action;
+                                console.log(response.result);
+
+                console.log("action: " + action);
+                command.callFunctionByName(action, response.result.parameters.functionType);
+            }
+        }
     });
 
     request.on('error', function(error) {
@@ -50,11 +65,20 @@ app.get('*', function(req, res) {
 // Express route to handle errors
 app.use(function(err, req, res, next) {
   if (req.xhr) {
-    res.status(500).send('Oops, Something went wrong!');
+    errorResponse(res);
   } else {
     next(err);
   }
 });
+
+function errorResponse(res, msg){
+    if (msg != undefined) {
+      res.status(500).send(msg);
+    } else {
+      res.status(500).send('Oops, Something went wrong!');
+    }
+    res.end();
+}
 
 app.listen(3000);
 console.log('App Server running at port 3000');
